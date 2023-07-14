8632e20aa856f8b6f5e12b597a5bea6b5b23aabf
Caylin James <caylinjames.dev@gmail.com>
2023-07-13 20:07:17 -0400

    set up an action creator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+const searchRepositories = (term: string) => {
+    return async (dispatch: any) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES,
+        });
+
+        try {
+        } catch (err) {
+            if (err instanceof TypeError) {
+                dispatch({
+                    type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                    payload: err.message,
+                });
+            }
+        }
+    };
+};
