db8bf32a523be56cf56080131033bf566ff71ff8
Caylin James <caylinjames.dev@gmail.com>
2023-07-13 20:13:33 -0400

    apply some typings to dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
@@ -9,6 +10,23 @@ const searchRepositories = (term: string) => {
         });
 
         try {
+            const { data } = await axios.get(
+                "https://registry.npmjs.org/-/v1/search",
+                {
+                    params: {
+                        text: term,
+                    },
+                }
+            );
+
+            const names = data.objects.map((result: any) => {
+                return result.package.name;
+            });
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names,
+            });
         } catch (err) {
             if (err instanceof TypeError) {
                 dispatch({
